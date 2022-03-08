b049794b558c1905d6ca0376981fb85da92e76db
ray <gang.wang05@sap.com>
2022-03-08 21:40:37 +0800

    增加一些 机器学习的概念

---
+++ code/SimpleLinearRegression1.py
@@ -0,0 +1,12 @@
+# @Time:2022/2/26 4:04 PM
+# @Author:Ray
+# @File:SimpleLinearRegression1.py
+
+#
+class SimpleLinearRegression1:
+    def __init__(self):
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        pass
