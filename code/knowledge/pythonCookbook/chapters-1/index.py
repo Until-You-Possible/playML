bf0f12b800b6363c19757e663e73ca457717bc53
ray <gang.wang05@sap.com>
2022-03-10 23:55:27 +0800

    增加ML的基础代码

---
+++ code/knowledge/pythonCookbook/chapters-1/index.py
@@ -0,0 +1,21 @@
+# @Time:2022/3/10 11:36 PM
+# @Author:Ray
+# @File:index.py.py
+import numpy as np
+
+
+# create a vector as a row
+def create_vector():
+    vector_row = np.array([1, 3, 3, 4, 5])
+
+
+# create a vector as a column
+def crete_vector_column():
+    vector_column = np.array([[1],
+                              [1],
+                              [3],
+                              [4]])
+
+
+def create_matrix():
+    matrix = np.array([[1, 2], [2, 3]])
