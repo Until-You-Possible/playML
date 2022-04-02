b105e33c65df30890ebace8fc46a541a552e9fa4
ray <gang.wang05@sap.com>
2022-04-02 21:27:17 +0800

    增加一些模型的注释

--- code/LinearRegression.py
+++ code/LinearRegression.py
@@ -9,7 +9,9 @@ import numpy as np
 
 class LinearRegression:
     def __init__(self):
+        # 系数
         self.coef = None
+        # 截距
         self.interception = None
         self._theta = None
 
@@ -18,6 +20,7 @@ class LinearRegression:
         根据训练数据集x_train y_train训练 Linear Regression模型
         """
         assert X_train.shape[0] == y_train.shape[0], "the size of X_train must be equal to the size of y_train"
+        # 关于数据公式转化代码 多翻numpy的doc
         X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
         self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
         self.interception = self._theta[0]
@@ -25,7 +28,7 @@ class LinearRegression:
         return self
 
     def predict(self, X_predict):
-        """给定待预测的数据集 X_predict，返回表示X_predict的结果向量 """
+        """ 给定待预测的数据集 X_predict，返回表示X_predict的结果向量 """
         assert self.interception is not None and self.coef is not None, "Must fit before predict"
         assert X_predict.shape[1] == len(self.coef), "the feature number of X_predict must be equal to X_train"
         X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
